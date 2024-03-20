fa41c99b5a8d021f9c27f1f2a2bbabaec5c3381b
WiscasHubble <LUIS.ACEVEDOSL@uanl.edu.mx>
2024-03-20 01:38:00 -0600

    - Luis Acevedo / WiscasHubble -----[Cambios]----- Nuevas carpetas: Administrador || Img || Plantillas || Usuario
    - bootstrap.min.css (Agregado en la carpeta "Styles")
    - Link del JS de bootstrap hasta el fondo del Body del Header
    - Logo de tornado en la carpeta de Img (noto-v1_tornado.png)
    - Header en la carpeta "Plantillas"

---
+++ Index.php
@@ -0,0 +1,3 @@
+<?php
+include './Plantillas/Header.php';
+?>
---
+++ Plantillas/Header.php
@@ -0,0 +1,83 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tatsu</title>
+    <link rel="Stylesheet" href="./Styles/bootstrap.min.css" />
+    <link rel="Stylesheet" href="./Styles/Style.css" />
+</head>
+
+<body>
+    <!-- Aquí comienza el encabezado ó "HEADER" -->
+    <header class="navbar navbar-expand navbar-light bg-light">
+        <ul class="nav navbar-nav">
+            <li class="nav-item">
+                <a class="navbar-brand" href="#">
+                    <img src="./Img/noto-v1_tornado.png" alt="Tatsu logo">Tatsu
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <div class="btn-group">
+                    <button type="button" class="btn btn-secondary">Etiquetas</button>
+                    <button type="button" class="btn btn-secondary dropdown-toggle dropdown-toggle-split"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        <span class="visually-hidden">Toggle Dropdown</span>
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">Separated link</a></li>
+                    </ul>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">Directorio de Mangas</a>
+            </li>
+            </div>
+            </li>
+
+            <li class="nav-item">
+                <a class="navbar-brand" href="#">
+                    <img src="./Img/Diamante.png" alt="LogoPremium">Premium
+                </a>
+            </li>
+                
+                
+                <li class="nav-item">
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="" id="" placeholder="Buscar">
+
+                    </div>
+                </li>
+
+                <li class="nav-item ">
+                    <a class="navbar-brand" href="#">
+                        <img src="./Img/Favoritos.png" alt="LogoFavoritos">
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="navbar-brand" href="#">
+                        <img src="./Img/Usuario.png" alt="LogoUsuario">
+                    </a>
+                </li>
+                
+            
+        </ul>
+
+    </header>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+
+</body>
+
+</html>
