737c1fd60c215302b993908fe5679a8a6095c5db
Arturro-es <emiliano.esquivela@uanl.edu.mx>
2024-03-26 19:58:40 -0600

    Esquivel Alvarez

--- login.php
+++ login.php
@@ -7,10 +7,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="Stylesheet" href="./Styles/bootstrap.min.css" />
     <link rel="Stylesheet" href="./Styles/normalize.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
   </head>
   <body>
-      
     <div class="container">
         <div class="row">
             <div class="col-md-4">
@@ -39,6 +39,5 @@
             
         </div>
     </div>
-
   </body>
 </html>
