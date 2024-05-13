a0d7bdf3f752da61ea9c8c9398eab0813b048789
WiscasHubble <luis.acevedosl@uanl.edu.mx>
2024-05-13 14:08:10 -0600

    Redireccionamientos corregidos (Header.php)

--- Plantillas/Header.php
+++ Plantillas/Header.php
@@ -11,7 +11,13 @@ if (!isset($_SESSION['IdUsuario'])) {
 // Si el usuario está autenticado, obtener el ID de usuario de la sesión
 $id_usuario = $_SESSION['IdUsuario'];
 
+
+
 ob_start();
+
+$url="http://".$_SERVER['HTTP_HOST']."/PROYECTO-FINAL-Pagina-Web-"; 
+// Esto sirve para redireccionar a la carpeta principal del proyecto (por el momento se llama "PROYECTO-FINAL-Pagina-Web-"), ['HTTP_HOST'] sirve para colocar al principio el nombre del host actual (por el momento el host es "localhost"), esto para que si lo llegamos a subir y le cambiamos el nombre al host por algo como "Tatsu.com" ahora este sea el nombre del HOST y no haya inconvenientes con páginas que no se ven porque el direccionamiento está incorrecto
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -36,14 +42,12 @@ ob_start();
 
 <body>
 
-<?php $url="http://".$_SERVER['HTTP_HOST']."/PROYECTO-FINAL-Pagina-Web-"; 
-// Esto sirve para redireccionar a la carpeta principal del proyecto (por el momento se llama "PROYECTO-FINAL-Pagina-Web-"), ['HTTP_HOST'] sirve para colocar al principio el nombre del host actual (por el momento el host es "localhost"), esto para que si lo llegamos a subir y le cambiamos el nombre al host por algo como "Tatsu.com" ahora este sea el nombre del HOST y no haya inconvenientes con páginas que no se ven porque el direccionamiento está incorrecto
-?>
+
     <div class="header-est">
         <!-- Encabezado de la página -->
         <nav class="navbar navbar-expand-lg navbar-light  header estetic">
             <div class="container-fluid">
-                <a class="navbar-brand  link-light" href="<?php echo $url;?>"> <!-- Al dar clic a Tatsu se redirecciona a la página principal del proyecto que vendría siendo la siguiente POR EL MOMENTO: "http://localhost/PROYECTO-FINAL-Pagina-Web-" -->
+                <a class="navbar-brand  link-light" href="<?php echo $url . '/Usuario/biblioteca.php';?>"> <!-- Al dar clic a Tatsu se redirecciona a la página principal del proyecto que vendría siendo la siguiente POR EL MOMENTO: "http://localhost/PROYECTO-FINAL-Pagina-Web-" -->
                     <img src="../Img/noto-v1_tornado.png" alt="Tatsu logo">Tatsu
                     <img class="container--img-anime" width="64" height="64"
                         src="https://img.icons8.com/nolan/64/kuromi.png" alt="kuromi" />
@@ -57,18 +61,18 @@ ob_start();
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 ul-center">
                         <img class="container-ul-img-sb" src="./Img/sombra.png" alt="">
                         <li class="nav-item">
-                            <a class="nav-link link-light" href="#">Directorio de Mangas</a>
+                            <a class="nav-link link-light" href="<?php echo $url . '/Usuario/biblioteca.php'; ?>">Directorio de Mangas</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link link-margin link-light" href="Plantillas\Premium.php">Premium
+                            <a class="nav-link link-margin link-light" href="<?php echo $url . 'Plantillas/Premium.php'; ?>">Premium
                                 <img class="img-premium nav-link" src="../Img/Diamante.png" alt="LogoPremium">
                             </a>
                         </li>
                         <li class="nav-item tm">
-                            <a class="nav-link link-red link-light  " href="#">Favoritos</a>
+                            <a class="nav-link link-red link-light  " href="<?php echo $url; ?>">Favoritos</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link link-light " href="Usuario/Mi cuenta.php">Usuario</a>
+                            <a class="nav-link link-light " href="<?php echo $url . '/Usuario/Mi cuenta.php'; ?>">Usuario</a>
                         </li>
 
                         <li class="nav-item dropdown ">
